e0c5bdb48fc76f57d589eced09f0c0f73eae4127
Elton Luiz Scholze <elscholze@gmail.com>
2022-12-22 19:41:44 -0300

    feat: check if user is active to login

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { userRepository } from "../repositories/userRepository";
+import { AppError } from "../errors/errors";
 
 const loginService = async (email: string) => {
     const user = await userRepository.findOneByOrFail({email: email});
+
+    if(!user.isActive) {
+        throw new AppError(400, "User is not active");
+    }
     
     const token = jwt.sign(
         { 
