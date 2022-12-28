7bdc7c875d19a06db1d41675b76cea6ab9794a79
Elton Luiz Scholze <elscholze@gmail.com>
2022-12-28 18:19:17 -0300

    feat: created created GET route to list the scheduled times of a property, fix: fixed timestamp checks with query builder and code reorganization in middlewares

--- src/middlewares/schedulesMiddlewares.ts
+++ src/middlewares/schedulesMiddlewares.ts
@@ -1,21 +1,82 @@
-// import { NextFunction, Request, Response } from "express";
-// import { propertyRepository } from "../repositories/propertyRepository";
-// import { scheduleRepository } from "../repositories/schedulesRepository";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/errors";
+import { propertyRepository } from "../repositories/propertyRepository";
+import { scheduleRepository } from "../repositories/schedulesRepository";
 
-// const verifyIfPropertyExists = async (req: Request, res: Response, next: NextFunction) => {
+const verifyIfScheduleExists = async (req: Request, res: Response, next: NextFunction) => {
+    const { propertyId } = req.body;
+    const userId = req.user.id;
 
+    const findScheduleExists = await scheduleRepository.findOne({
+        where: { property: { id: propertyId } }
+    });
+
+    const schedules = await scheduleRepository.createQueryBuilder("schedules")
+    .innerJoinAndSelect("schedules.user", "user")
+    .innerJoinAndSelect("schedules.property", "property")
+    .where("property.id = :id_property", { id_property: propertyId })
+    .select(["schedules", "user.name", "user.email", "user.id", "property"])
+    .getMany()
+
+    const schedulesEquals = await scheduleRepository.createQueryBuilder("schedules")
+    .innerJoinAndSelect("schedules.user", "user")
+    .innerJoinAndSelect("schedules.property", "property")
+    .where("user.id = :id_user", { id_user: userId })
+    .select(["schedules", "user.name", "user.email", "user.id", "property"])
+    .getMany()
     
+    schedules.find((schedule) => {
+        if(schedule.hour === findScheduleExists.hour && schedule.date === findScheduleExists.date) {
+            throw new AppError(409,  "Property schedule already exists");
+        }
 
-//     console.log(propertyId)
+        schedulesEquals.filter((scheduleEqual) => {
+            if(scheduleEqual.hour === schedule.hour && scheduleEqual.date === schedule.date && userId === schedule.user.id) {
+                throw new AppError(409,  "User schedule already exists");
+            }
+        });
+    })
+
+    return next();
+}
+
+const verifyIfPropertyExists = async (req: Request, res: Response, next: NextFunction) => {
+    const { propertyId } = req.body;
+
+    const findProperty = await propertyRepository.findOneBy({ id: propertyId });
     
-//     const schedules = await scheduleRepository.createQueryBuilder("schedules")
-//     .innerJoinAndSelect("schedules.property", "property")
-//     .innerJoinAndSelect("properties.schedules", "schedules")
-//     .where("property.id = :id_property", { id_property: propertyId })
-//     .getMany()
+    if(!findProperty) {
+        throw new AppError(404, "Property not found");
+    }
 
-//     console.log(schedules)
-//     return next()
-// }
+    return next();
+}
 
-// export { verifyIfPropertyExists };
+const verifyIfHourIsInvalidMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const { hour } = req.body
+    const splitHour = hour.split(":");
+    const newHour = splitHour[0];
+
+    if(newHour < "08" || newHour >= "18") {
+        throw new AppError(400, "Invalid hour");
+    }
+
+    req.newHour = newHour
+
+    return next();
+}
+
+const verifyIfDateIsInvalidMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const { date } = req.body;
+    const newDate = new Date(date);
+    const dayOfTheWeek = newDate.getDay();
+    
+    
+    if(dayOfTheWeek === 0 || dayOfTheWeek === 6) {
+        throw new AppError(400, "Invalid Date");
+    }
+
+    return next();
+}
+
+export { verifyIfScheduleExists, verifyIfPropertyExists, verifyIfHourIsInvalidMiddleware, verifyIfDateIsInvalidMiddleware };
--- src/services/schedulesServices.ts
+++ src/services/schedulesServices.ts
@@ -3,43 +3,14 @@ import { IScheduleRequest } from "../interfaces/schedules";
 import { propertyRepository } from "../repositories/propertyRepository";
 import { scheduleRepository } from "../repositories/schedulesRepository";
 
-const createScheduleService = async (data: IScheduleRequest, id: string) => {
-    const { date, hour, propertyId } = data;
+const createScheduleService = async (data: IScheduleRequest, hour: string, userId: string) => {
+    const {date, propertyId } = data;
  
-    const newDate = new Date(data.date);
-    const dayOfTheWeek = newDate.getDay();
-    const splitHour = data.hour.split(":");
-    const newHour = splitHour[0];
-    const findProperty = await propertyRepository.findOneBy({ id: propertyId });
-
-    const schedules = await scheduleRepository.createQueryBuilder("schedules")
-    .innerJoinAndSelect("schedules.user", "user")
-    .innerJoinAndSelect("schedules.property", "property")
-    .where("property.id = :id_property", { id_property: propertyId })
-    .select(["schedules", "user", "property"])
-    .getRawMany();
-    
-    if(dayOfTheWeek === 0 || dayOfTheWeek === 6) {
-        throw new AppError(400, "Invalid Date");
-    }
-    
-    if(newHour < "08" || newHour >= "18") {
-        throw new AppError(400, "Invalid hour");
-    }
-    
-    if(!findProperty) {
-        throw new AppError(404, "Property not found");
-    }
-
-    if(schedules.length > 1) {
-        throw new AppError(409,  "User schedule already exists") 
-    }
-
     const newSchedule = scheduleRepository.create({
         date: date, 
         hour: hour, 
         property: {id: propertyId},
-        user: {id: id}
+        user: {id: userId}
     })
     
     await scheduleRepository.save(
@@ -47,6 +18,27 @@ const createScheduleService = async (data: IScheduleRequest, id: string) => {
     );
 
     return {message: "Schedule created"};
+
 }
 
-export { createScheduleService };
+const listAllSchedulesByPropertyIdService = async (id: string) => {
+    const findProperty = await propertyRepository.findOneBy({ id: id });
+
+    if(!findProperty) {
+        throw new AppError(404, "Property not found");
+    }    
+
+    const allSchedules = await propertyRepository.findOne({ where : { id: id },
+        relations: {
+            address: true,
+            category: true,
+            schedules: {
+                user: true
+            }
+        }
+    })
+
+    return allSchedules;
+}
+
+export { createScheduleService, listAllSchedulesByPropertyIdService };
